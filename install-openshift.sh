71c301d56fb0f56b6bb5a46aaa1a5bbc45f75743
samuka182 <samuka41@gmail.com>
2019-01-01 21:44:21 -0200

    Update install-openshift.sh

--- install-openshift.sh
+++ install-openshift.sh
@@ -152,8 +152,8 @@ fi
 mkdir -p /etc/origin/master/
 touch /etc/origin/master/htpasswd
 
-ansible-playbook -i inventory.ini openshift-ansible/playbooks/prerequisites.yml
-ansible-playbook -i inventory.ini openshift-ansible/playbooks/deploy_cluster.yml
+ansible-playbook -i inventory.ini openshift-ansible/playbooks/prerequisites.yml --private-key=~/.ssh/authorized_keys
+ansible-playbook -i inventory.ini openshift-ansible/playbooks/deploy_cluster.yml --private-key=~/.ssh/authorized_keys
 
 htpasswd -b /etc/origin/master/htpasswd ${USERNAME} ${PASSWORD}
 oc adm policy add-cluster-role-to-user cluster-admin ${USERNAME}
